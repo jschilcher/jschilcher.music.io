4ef84ad9d394f67159017f91147d734ecc72a49e
Jenelle Schilcher <jschilcher8@gmail.com>
2021-06-16 20:01:50 -0500

    Fix cd package.json

---
+++ music-library/src/components/MusicTable/musicTable.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function MusicTable(props){
+    return(
+        console.log("music table!")
+    );
+}
+
+export default MusicTable;
--- music-library/src/components/app.jsx
+++ music-library/src/components/app.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
+import axios from "axios";
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            musicData: []
+        }
 
     }
+
+    componentDidMount(){
+        this.getMusicData();
+    }
+
+    async getMusicData(){
+        let response = await axios.get("http://www.devcodecampmusiclibrary.com/api/music");
+        this.setState({
+            musicData: response.data
+        });
+    }
+
     render(){
         return(
             <div></div>
