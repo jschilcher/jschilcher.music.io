7643521cb69d7dca038fd831defd3fe56e38662e
Jenelle Schilcher <jschilcher8@gmail.com>
2021-06-17 19:34:46 -0500

    Display table

--- music-library/src/components/MusicTable/musicTable.jsx
+++ music-library/src/components/MusicTable/musicTable.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function MusicTable(props){
-    // let allMusicData = props.data.map((music) => {}
+    // let allMusicData = props.data.map((music) => {
+
+    // })
     return(
         <table class="table">
   <thead>
@@ -15,22 +17,173 @@ function MusicTable(props){
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    {/* <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">{props.data[0].title}</th>
+      <td>{props.data[0].album}</td>
+      <td>{props.data[0].artist}</td>
+      <td>{props.data[0].genre}</td>
+      <td>{props.data[0].releaseDate}</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr> */}
+      <th scope="row">{props.data[1].title}</th>
+      <td>{props.data[1].album}</td>
+      <td>{props.data[1].artist}</td>
+      <td>{props.data[1].genre}</td>
+      <td>{props.data[1].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[2].title}</th>
+      <td>{props.data[2].album}</td>
+      <td>{props.data[2].artist}</td>
+      <td>{props.data[2].genre}</td>
+      <td>{props.data[2].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[3].title}</th>
+      <td>{props.data[3].album}</td>
+      <td>{props.data[3].artist}</td>
+      <td>{props.data[3].genre}</td>
+      <td>{props.data[3].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[4].title}</th>
+      <td>{props.data[4].album}</td>
+      <td>{props.data[4].artist}</td>
+      <td>{props.data[4].genre}</td>
+      <td>{props.data[4].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[5].title}</th>
+      <td>{props.data[5].album}</td>
+      <td>{props.data[5].artist}</td>
+      <td>{props.data[5].genre}</td>
+      <td>{props.data[5].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[6].title}</th>
+      <td>{props.data[6].album}</td>
+      <td>{props.data[6].artist}</td>
+      <td>{props.data[6].genre}</td>
+      <td>{props.data[6].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[7].title}</th>
+      <td>{props.data[7].album}</td>
+      <td>{props.data[7].artist}</td>
+      <td>{props.data[7].genre}</td>
+      <td>{props.data[7].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[8].title}</th>
+      <td>{props.data[8].album}</td>
+      <td>{props.data[8].artist}</td>
+      <td>{props.data[8].genre}</td>
+      <td>{props.data[8].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[9].title}</th>
+      <td>{props.data[9].album}</td>
+      <td>{props.data[9].artist}</td>
+      <td>{props.data[9].genre}</td>
+      <td>{props.data[9].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[10].title}</th>
+      <td>{props.data[10].album}</td>
+      <td>{props.data[10].artist}</td>
+      <td>{props.data[10].genre}</td>
+      <td>{props.data[10].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[11].title}</th>
+      <td>{props.data[11].album}</td>
+      <td>{props.data[11].artist}</td>
+      <td>{props.data[11].genre}</td>
+      <td>{props.data[11].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[12].title}</th>
+      <td>{props.data[12].album}</td>
+      <td>{props.data[12].artist}</td>
+      <td>{props.data[12].genre}</td>
+      <td>{props.data[12].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[13].title}</th>
+      <td>{props.data[13].album}</td>
+      <td>{props.data[13].artist}</td>
+      <td>{props.data[13].genre}</td>
+      <td>{props.data[13].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[14].title}</th>
+      <td>{props.data[14].album}</td>
+      <td>{props.data[14].artist}</td>
+      <td>{props.data[14].genre}</td>
+      <td>{props.data[14].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[15].title}</th>
+      <td>{props.data[15].album}</td>
+      <td>{props.data[15].artist}</td>
+      <td>{props.data[15].genre}</td>
+      <td>{props.data[15].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[16].title}</th>
+      <td>{props.data[16].album}</td>
+      <td>{props.data[16].artist}</td>
+      <td>{props.data[16].genre}</td>
+      <td>{props.data[16].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[17].title}</th>
+      <td>{props.data[17].album}</td>
+      <td>{props.data[17].artist}</td>
+      <td>{props.data[17].genre}</td>
+      <td>{props.data[17].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[18].title}</th>
+      <td>{props.data[18].album}</td>
+      <td>{props.data[18].artist}</td>
+      <td>{props.data[18].genre}</td>
+      <td>{props.data[18].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[19].title}</th>
+      <td>{props.data[19].album}</td>
+      <td>{props.data[19].artist}</td>
+      <td>{props.data[19].genre}</td>
+      <td>{props.data[19].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[20].title}</th>
+      <td>{props.data[20].album}</td>
+      <td>{props.data[20].artist}</td>
+      <td>{props.data[20].genre}</td>
+      <td>{props.data[20].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[21].title}</th>
+      <td>{props.data[21].album}</td>
+      <td>{props.data[21].artist}</td>
+      <td>{props.data[21].genre}</td>
+      <td>{props.data[21].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[22].title}</th>
+      <td>{props.data[22].album}</td>
+      <td>{props.data[22].artist}</td>
+      <td>{props.data[22].genre}</td>
+      <td>{props.data[22].releaseDate}</td>
+    </tr>
+    <tr>
+      <th scope="row">{props.data[23].title}</th>
+      <td>{props.data[23].album}</td>
+      <td>{props.data[23].artist}</td>
+      <td>{props.data[23].genre}</td>
+      <td>{props.data[23].releaseDate}</td>
+    </tr>
   </tbody>
 </table>
     );
--- music-library/src/components/app.jsx
+++ music-library/src/components/app.jsx
@@ -1,36 +1,43 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import TitleBar from "./TitleBar/titleBar";
 import MusicTable from "./MusicTable/musicTable";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            musicData: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      musicData: [],
+    };
+  }
 
-    }
+  componentDidMount() {
+    this.getMusicData();
+  }
 
-    componentDidMount(){
-        this.getMusicData();
-    }
+  async getMusicData() {
+    let response = await axios.get(
+      "http://www.devcodecampmusiclibrary.com/api/music"
+    );
+    this.setState({
+      musicData: response.data,
+    });
+  }
 
-    async getMusicData(){
-        let response = await axios.get("http://www.devcodecampmusiclibrary.com/api/music");
-        this.setState({
-            musicData: response.data
-        });
+  render() {
+    if (this.state.musicData[0] === undefined) {
+      return <div></div>;
+    } else {
+      return (
+        <div>
+          <TitleBar/>
+          <MusicTable data={this.state.musicData}/>
+          <h1>{this.state.musicData[0].title}</h1>
+                      {/* {this.state.musicData ? (<MusicTable data={this.state.musicData} />) : (<h1>Loading....</h1>)} */}
+        </div>
+      );
     }
-
-    render(){
-        return(
-            <div>
-                <TitleBar/>
-                {this.state.musicData ? (<MusicTable data={this.state.musicData} />) : (<h1>Loading....</h1>)}
-            </div>
-        );
-    }
-}    
+  }
+}
 
 export default App;
